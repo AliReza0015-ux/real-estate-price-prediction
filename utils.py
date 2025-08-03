5d6b66c762bfb037bca180945b2c096caea36d24
Ali Reza <mohsalireza@gmail.com>
2025-08-03 18:01:18 -0400

    Fix input format to match model's expected feature names

--- utils.py
+++ utils.py
@@ -1,5 +1,13 @@
+import pandas as pd
+
 def preprocess_input(year, beds, baths, stories, garage, area, lot, pool, fireplace, renovated, rating, condo, bunglow):
-    return [[
+    columns = [
+        'year', 'beds', 'baths', 'stories', 'garage',
+        'area', 'lot', 'pool', 'fireplace', 'renovated',
+        'rating', 'property_type_Condo', 'property_type_Bunglow'
+    ]
+    values = [[
         year, beds, baths, stories, garage, area, lot, pool,
         fireplace, renovated, rating, condo, bunglow
     ]]
+    return pd.DataFrame(values, columns=columns)
